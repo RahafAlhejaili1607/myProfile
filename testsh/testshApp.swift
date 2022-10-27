93a1ddeb4c22a2dbc7bdf0bc8d771cd7eebcde3b
RahafAlhejaili1607 <Rbalhejaili22@twq.idserve.net>
2022-10-27 11:43:25 +0300

    Initial Commit

---
+++ testsh/testshApp.swift
@@ -0,0 +1,17 @@
+//
+//  testshApp.swift
+//  testsh
+//
+//  Created by Rahaf Alhejaili on 02/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct testshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
